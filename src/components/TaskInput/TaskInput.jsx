1dd8a309b6663c6874bb0a0448df744b634b0b40
viktoriagrubi <wiktoria.grubizna@op.pl>
2025-08-26 09:15:17 +0200

    update task input

--- src/components/TaskInput/TaskInput.jsx
+++ src/components/TaskInput/TaskInput.jsx
@@ -3,24 +3,29 @@ import styles from "./TaskInput.module.css";
 
 function TaskInput() {
   const [task, setTask] = useState("");
-  const handleSubmit = (e) => {
-    e.preventDefault();
+
+  const handleEnterPress = (e) => {
+    if (e.key !== "Enter") {
+      return;
+    }
     if (!task.trim()) return;
     setTask("");
   };
+
   return (
-    <form onSubmit={handleSubmit} className={styles.container}>
+    <div className={styles.container}>
       <div className={styles.inputWrapper}>
         <div className={styles.circle}></div>
         <input
           type="text"
           value={task}
           onChange={(e) => setTask(e.target.value)}
+          onKeyUp={handleEnterPress}
           placeholder="Currently typing"
           className={styles.input}
         />
       </div>
-    </form>
+    </div>
   );
 }
 
